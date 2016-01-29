2899a38fb18e639a4b16be8d2cc19b9a06d8e555
John Paul Borrego <johnpaul@JPB.local>
2016-01-28 23:28:07 -0600

    added file to repository

--- Delete this copycat/ViewController.swift
+++ Delete this copycat/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BlueBG: UIImageView!
+    @IBOutlet weak var RedBG: UIImageView!
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var HideBlue: UIButton!
+    @IBOutlet weak var HideRed: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideBlue(sender: AnyObject) {
+        BlueBG.hidden = true
+        RedBG.hidden = false
+        Logo.hidden = false
+        HideBlue.hidden = true
+        HideRed.hidden = false
+        
+    }
+    
+    @IBAction func HideRed(sender: AnyObject) {
+        BlueBG.hidden = false
+        RedBG.hidden = true
+        Logo.hidden = false
+        HideRed.hidden = true
+        HideBlue.hidden = false
+        
+    }
+    
+    
+    
+    
+    
 
 }
 
